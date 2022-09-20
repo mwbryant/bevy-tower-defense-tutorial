3388776a6d425089ddd6d72bce8a0b2fbb630c7b
Matthew <logicprojectsforfpgas@gmail.com>
2022-09-20 15:46:31 -0600

    Basic gameplay added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::f32::consts::PI;
-
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::FloatOrd};
 use bevy_inspector_egui::WorldInspectorPlugin;
 
 pub const HEIGHT: f32 = 720.0;
@@ -10,12 +8,32 @@ pub const WIDTH: f32 = 1280.0;
 #[reflect(Component)]
 pub struct Tower {
     shooting_timer: Timer,
+    bullet_offset: Vec3,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Bullet {
+    direction: Vec3,
+    speed: f32,
 }
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
 pub struct Lifetime {
     timer: Timer,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Target {
+    speed: f32,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Health {
+    value: i32,
 }
 
 pub struct GameAssets {
@@ -38,11 +56,18 @@ fn main() {
         .add_plugin(WorldInspectorPlugin::new())
         .register_type::<Tower>()
         .register_type::<Lifetime>()
+        .register_type::<Target>()
+        .register_type::<Bullet>()
+        .register_type::<Health>()
         // Our Systems
         .add_startup_system(spawn_basic_scene)
         .add_startup_system(spawn_camera)
         .add_startup_system(asset_loading)
         .add_system(tower_shooting)
+        .add_system(target_death)
+        .add_system(bullet_collision)
+        .add_system(move_targets)
+        .add_system(move_bullets)
         .add_system(bullet_despawn)
         .run();
 }
@@ -66,29 +91,80 @@ fn bullet_despawn(
     }
 }
 
+fn target_death(mut commands: Commands, targets: Query<(Entity, &Health)>) {
+    for (ent, health) in &targets {
+        if health.value <= 0 {
+            commands.entity(ent).despawn_recursive();
+        }
+    }
+}
+
 fn tower_shooting(
     mut commands: Commands,
-    mut towers: Query<&mut Tower>,
+    mut towers: Query<(Entity, &mut Tower, &GlobalTransform)>,
+    targets: Query<&GlobalTransform, With<Target>>,
     bullet_assets: Res<GameAssets>,
     time: Res<Time>,
 ) {
-    for mut tower in &mut towers {
+    for (tower_ent, mut tower, transform) in &mut towers {
         tower.shooting_timer.tick(time.delta());
         if tower.shooting_timer.just_finished() {
-            let spawn_transform =
-                Transform::from_xyz(0.0, 0.7, 0.6).with_rotation(Quat::from_rotation_y(-PI / 2.0));
+            let bullet_spawn = transform.translation() + tower.bullet_offset;
 
-            commands
-                .spawn_bundle(SceneBundle {
-                    scene: bullet_assets.bullet_scene.clone(),
-                    transform: spawn_transform,
-                    ..Default::default()
+            let direction = targets
+                .iter()
+                .min_by_key(|target_transform| {
+                    FloatOrd(Vec3::distance(target_transform.translation(), bullet_spawn))
                 })
-                .insert(Lifetime {
-                    timer: Timer::from_seconds(0.5, false),
-                })
-                .insert(Name::new("Bullet"));
+                .map(|closest_target| closest_target.translation() - bullet_spawn);
+
+            if let Some(direction) = direction {
+                commands.entity(tower_ent).with_children(|commands| {
+                    commands
+                        .spawn_bundle(SceneBundle {
+                            scene: bullet_assets.bullet_scene.clone(),
+                            transform: Transform::from_translation(tower.bullet_offset),
+                            ..Default::default()
+                        })
+                        .insert(Lifetime {
+                            timer: Timer::from_seconds(1000.5, false),
+                        })
+                        .insert(Bullet {
+                            direction,
+                            speed: 2.5,
+                        })
+                        .insert(Name::new("Bullet"));
+                });
+            }
         }
+    }
+}
+
+fn move_bullets(mut bullets: Query<(&Bullet, &mut Transform)>, time: Res<Time>) {
+    for (bullet, mut transform) in &mut bullets {
+        transform.translation += bullet.direction.normalize() * bullet.speed * time.delta_seconds();
+    }
+}
+
+fn bullet_collision(
+    mut commands: Commands,
+    bullets: Query<(Entity, &GlobalTransform), With<Bullet>>,
+    mut targets: Query<(&mut Health, &Transform), With<Target>>,
+) {
+    for (bullet, bullet_transform) in &bullets {
+        for (mut health, target_transform) in &mut targets {
+            if Vec3::distance(bullet_transform.translation(), target_transform.translation) < 0.2 {
+                commands.entity(bullet).despawn_recursive();
+                health.value -= 1;
+                break;
+            }
+        }
+    }
+}
+
+fn move_targets(mut targets: Query<(&Target, &mut Transform)>, time: Res<Time>) {
+    for (target, mut transform) in &mut targets {
+        transform.translation.x += target.speed * time.delta_seconds();
     }
 }
 
@@ -114,8 +190,31 @@ fn spawn_basic_scene(
         })
         .insert(Tower {
             shooting_timer: Timer::from_seconds(1.0, true),
+            bullet_offset: Vec3::new(0.0, 0.2, 0.5),
         })
         .insert(Name::new("Tower"));
+
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 0.4 })),
+            material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
+            transform: Transform::from_xyz(-2.0, 0.2, 1.5),
+            ..default()
+        })
+        .insert(Target { speed: 0.3 })
+        .insert(Health { value: 3 })
+        .insert(Name::new("Target"));
+
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 0.4 })),
+            material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
+            transform: Transform::from_xyz(-4.0, 0.2, 1.5),
+            ..default()
+        })
+        .insert(Target { speed: 0.3 })
+        .insert(Health { value: 3 })
+        .insert(Name::new("Target"));
 
     commands
         .spawn_bundle(PointLightBundle {
