c7c96a887d80dc7e83d0b53ec7afb0ec85c17a05
Matthew <logicprojectsforfpgas@gmail.com>
2022-11-14 11:03:53 -0700

    Update to bevy 0.9

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,18 @@ pub const WIDTH: f32 = 1280.0;
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
-        .insert_resource(WindowDescriptor {
-            width: WIDTH,
-            height: HEIGHT,
-            title: "Bevy Tower Defense".to_string(),
-            resizable: false,
-            ..Default::default()
-        })
         .add_startup_system(spawn_basic_scene)
         .add_startup_system(spawn_camera)
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: WIDTH,
+                height: HEIGHT,
+                title: "Bevy Tower Defense".to_string(),
+                resizable: false,
+                ..Default::default()
+            },
+            ..default()
+        }))
         .run();
 }
 
@@ -24,13 +26,13 @@ fn spawn_basic_scene(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn_bundle(PbrBundle {
+    commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
 
-    commands.spawn_bundle(PbrBundle {
+    commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
@@ -39,7 +41,7 @@ fn spawn_basic_scene(
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn_bundle(Camera3dBundle {
+    commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
